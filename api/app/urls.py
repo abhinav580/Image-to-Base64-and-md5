809dc2beb72fbcb008c92c3a4c915d8a1b169257
Abhinav <abhinavkumar580@gmail.com>
2020-08-31 03:57:14 +0530

    Basic Api created

--- api/app/urls.py
+++ api/app/urls.py
@@ -1,7 +1,10 @@
 from django.contrib import admin
 from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
 from . import views
 urlpatterns = [
     path('', views.index, name="index"),
     path('upload',views.upload, name="upload"),
-]
+    path('api/', views.api, name="api"),
+]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- api/app/views.py
+++ api/app/views.py
@@ -1,7 +1,11 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
 import hashlib
 import base64
+import os
+from PIL import Image
+import urllib.request
+import requests
 # Create your views here.
 def index(request):
     return render(request, "index.html")
@@ -14,6 +18,31 @@ def upload(request):
 
     base64_bytes = base64.b64encode(bytes)
     base64_message = base64_bytes.decode('ascii')
+    json = {
+        "md5" : readable_hash,
+        "base64" : base64_message,
+    }
+    return JsonResponse(json)
 
-    mid = "data:image/png;base64," + base64_message
-    return render(request, "show.html", {"path":mid})
+def api(request):
+    url = request.GET.get('url')
+    res = requests.get(url)
+    if(res.status_code!=200):
+        return JsonResponse({"error":"Image not found"})
+    file = url.split("/")[-1]
+    res.raw.decode_content=True
+    p = open(file, "wb")
+    p.write(res.content)
+    p.close()
+    p = open(file, "rb")
+    bytes = p.read() # read file as bytes
+    readable_hash = hashlib.md5(bytes).hexdigest()
+
+    base64_bytes = base64.b64encode(bytes)
+    base64_message = base64_bytes.decode('ascii')
+    json = {
+        "md5" : readable_hash,
+        "base64" : base64_message,
+    }
+    os.remove(file)
+    return JsonResponse(json)
